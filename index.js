6b5327bf98e69fa5198563613074e0995d9ad156
ronniejuan <hofmannronnie@gmail.com>
2022-01-18 11:43:52 +0100

    baby steps eeaasy

---
+++ index.js
@@ -0,0 +1,25 @@
+const training = [
+  {Title: 'Swamp Music',
+  data:['track1','track2','track3']
+  },
+  {title: 'CodeMusic',
+  data: ['bar1','bar2','bar3'] 
+  },
+  {title:'Cheap Poetry',
+  data2: ['Vol1','Vol2','Vol3']
+  }
+];
+
+console.log(training)
+
+
+
+  let array1 = ['Swamp Music', 'CodeMusic','Cheap Poetry']
+  let array2 = ['track1','bar1','Vol1']
+  let array3 = ['track2','bar2','Vol2']
+  let array4 = ['track3','bar3','Vol3']
+
+
+let row =[ array1,array2, array3, array4]
+ console.log(row)
+
